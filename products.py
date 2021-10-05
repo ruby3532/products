d00241fa1eeb5143a448abd6cad15c81cecda1ee
李心汝 <lixinru@lixinrudeAir.Home>
2021-10-05 21:17:05 +0800

    add write csv and txt

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格： ') # 價格寫在這裡，是因為如果他沒有輸入商品名稱，就不需要輸入價格了。
+	price = int(price) # 價格變整數
 	p = []
 	p.append(name)
 	p.append(price)
@@ -18,4 +19,15 @@ print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+with open ('products.csv', 'w') as f:   # 寫入模式，用 f 稱做 products.txt 檔
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') # 用加法合併字串open。f.write 才是真的寫入
+# csv 是很常儲存數據的格式
+# with 是自動 close 的功能
+
+# 字串的加和乘法
+# 'abc' + '123' = 'abc123'
+# 'abc'* 3 = 'abcabcabc'
+
 
