57a420547113c193a34da153e0e2360d0d34214b
李心汝 <lixinru@lixinrudeAir.Home>
2021-10-10 22:32:17 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# 讀取檔案
 products = []
+with open('products.csv' , 'r' , encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue # 不會跳出迴圈，會直接跳到下一回
+		name, price = line.strip().split(',')
+		products.append([name , price])
+print(products)
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q':
@@ -17,9 +27,11 @@ print(products)
 
 # products[0][0] # product 中第 0 格中的第 0 格。
 
+# 印出所有購買記錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+# 寫入檔案
 with open ('products.csv', 'w', encoding = 'utf-8') as f:   # 寫入模式，用 f 稱做 products.txt 檔
 	f.write('商品,價格\n')
 	for p in products:
